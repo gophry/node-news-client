0ffebcf3f1e870668e4936de3509c7416e72130b
Yogesh Kamat <ykamat@gmail.com>
2014-11-25 13:38:18 +0400

    Basic HTML Page with bootstrap and angular

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,32 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
+var gulp = require('gulp'),
+gutil = require('gulp-util'),
+webserver = require('gulp-webserver');
 
-gulp.task('log',function(){
-
-  gutil.log('Workflow are awesome');
+gulp.task('js', function() {
+  gulp.src('app/dev/js/**/*')
 });
+
+gulp.task('html', function() {
+  gulp.src('app/dev/*.html')
+});
+
+gulp.task('css', function() {
+  gulp.src('app/dev/css/*.css')
+});
+
+gulp.task('watch', function() {
+  gulp.watch('app/dev/js/**/*', ['js']);
+  gulp.watch('app/dev/css/*.css', ['css']);
+  gulp.watch(['app/dev/*.html',
+  'app/dev/views/*.html'], ['html']);
+});
+
+gulp.task('webserver', function() {
+  gulp.src('app/dev/')
+  .pipe(webserver({
+    livereload: true,
+    open: true
+  }));
+});
+
+gulp.task('default', ['watch', 'html', 'js', 'css', 'webserver']);
