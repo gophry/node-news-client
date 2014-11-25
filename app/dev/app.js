139b3ad7efdb965b6afbecb9701da96bd1f0c2f4
Yogesh Kamat <ykamat@gmail.com>
2014-11-25 18:02:40 +0400

    Generate Application Header

--- app/dev/app.js
+++ app/dev/app.js
@@ -1,14 +1,24 @@
 (function(){
-  var gem = {
-    name: 'Honeywell',
-    description: 'My Brand',
-    contact: 'ykamat@gmail.com'
-  };
-  var app = angular.module('news', []);
-  app.controller('BrandController', function(){
-    this.brand = gem;
 
-    console.log('hello world');
+  // @companyName,@loggeduser === fetched from global json object name.
+  var loggedUser,companyName;
+
+  var app = angular.module('news', []);
+
+  //define a controller for navigation contents.
+  app.controller('NavController',['$http', function($http){
+    var globals = this;
+    globals.navigation = [ ];//initialize before page load;
+
+    $http.get('common/globals.json').success(function(data){
+      globals.navigation = data;//assign fetched data
+    });
+
+  }]);
+
+  //
+  app.controller('BrandController', function(){
+
   });
 
 })();
