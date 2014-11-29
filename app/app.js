48ec815aed1fbb816018ae2cc19b6a1e114d55e5
Yogesh Kamat <ykamat@gmail.com>
2014-11-29 16:33:31 +0400

    route state change in url to brand works.

--- app/app.js
+++ app/app.js
@@ -4,13 +4,10 @@
 
   //application level controller for initializing the application.
   app.controller('AppController',['$state','$log', function($state,$log){
-    console.log('Init');
     $log.info('Initializing Application...');//logging initialization.
     $state.go('home');//initialize the application and load the 'home' abstract state.
-
-
-
   }]);
+
   //temporary controller for pulling json data.
   app.controller('NavController',['$http', function($http){
     var globals = this;
@@ -24,21 +21,20 @@
 
   //application routes and states definition
   app.config(function($stateProvider,$urlRouterProvider){
-    //Define Top level helper layouts.
 
-    //'home' abstract state
+
+    //'home' abstract state for application layout.
     $stateProvider.state('home',{
-       url: '/',
-        views: {
-         'header': {
-            templateUrl: 'templates/header.html'
-          },
-          'sidebar': {
-            'templateUrl': 'templates/sidebar.html'
-          }
-        },
-      controller: 'NavController'
+        abstract: true,
+        controller: 'NavController'
     });
+    //'oute state for the brand module
+    $stateProvider.state('brand',{
+      url:'/brand',
+      templateUrl:'brand/brand.html',
+      controller:''
+    });
+
     //$urlRouterProvider.otherwise('/');
   });
 
